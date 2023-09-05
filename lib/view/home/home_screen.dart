5f67ad4376dd4e0198a78507fa7e250d8eedf365
Unnati Donda <134287519+unnatidonda@users.noreply.github.com>
2023-09-05 17:14:47 +0530

    changes

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -122,112 +122,112 @@ class _HomeScreenState extends State<HomeScreen> {
         icon: const Icon(Icons.add),
         label: const Text('Add To-do'),
       ),
-      Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 30),
-            child: Column(
-              children: [
-                Container(
-                  height: 280,
-                  width: double.infinity,
-                  color: Colors.greenAccent,
-                  child: Row(
-                    children: [
-                      const Column(
-                        children: [
-                          Text(
-                            textAlign: TextAlign.start,
-                            " # Student History : ",
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 26,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                          Text(
-                            textAlign: TextAlign.start,
-                            " 1 Surname : Donda \n      Name : Unnati  \n      Hobby : Traveling  \n      Study : BCA running",
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 26,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                          SizedBox(height: 50),
-                          Text(
-                            textAlign: TextAlign.start,
-                            "time:12.30 & 4.00",
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 26,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 80),
-                        child: PopupMenuButton<SampleItem>(
-                          initialValue: selectedMenu,
-                          // Callback that sets the selected popup menu item.
-                          onSelected: (SampleItem item) {
-                            setState(() {
-                              selectedMenu = item;
-                            });
-                          },
-                          itemBuilder: (BuildContext context) => <PopupMenuEntry<SampleItem>>[
-                            const PopupMenuItem<SampleItem>(
-                              value: SampleItem.itemOne,
-                              child: Text(
-                                "delete",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w700,
-                                  fontSize: 18,
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ),
-                            const PopupMenuItem<SampleItem>(
-                              value: SampleItem.itemTwo,
-                              child: Text(
-                                "Edite",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w700,
-                                  fontSize: 18,
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 400),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    const SizedBox(width: 280),
-                    FloatingActionButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const TodoScreen(),
-                          ),
-                        );
-                      },
-                      child: const Icon(Icons.add),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+      // Column(
+      //   children: [
+      //     Padding(
+      //       padding: const EdgeInsets.only(top: 30),
+      //       child: Column(
+      //         children: [
+      //           Container(
+      //             height: 280,
+      //             width: double.infinity,
+      //             color: Colors.greenAccent,
+      //             child: Row(
+      //               children: [
+      //                 const Column(
+      //                   children: [
+      //                     Text(
+      //                       textAlign: TextAlign.start,
+      //                       " # Student History : ",
+      //                       style: TextStyle(
+      //                         color: Colors.black,
+      //                         fontSize: 26,
+      //                         fontWeight: FontWeight.w700,
+      //                       ),
+      //                     ),
+      //                     Text(
+      //                       textAlign: TextAlign.start,
+      //                       " 1 Surname : Donda \n      Name : Unnati  \n      Hobby : Traveling  \n      Study : BCA running",
+      //                       style: TextStyle(
+      //                         color: Colors.black,
+      //                         fontSize: 26,
+      //                         fontWeight: FontWeight.w700,
+      //                       ),
+      //                     ),
+      //                     SizedBox(height: 50),
+      //                     Text(
+      //                       textAlign: TextAlign.start,
+      //                       "time:12.30 & 4.00",
+      //                       style: TextStyle(
+      //                         color: Colors.black,
+      //                         fontSize: 26,
+      //                         fontWeight: FontWeight.w700,
+      //                       ),
+      //                     ),
+      //                   ],
+      //                 ),
+      //                 Padding(
+      //                   padding: const EdgeInsets.only(left: 80),
+      //                   child: PopupMenuButton<SampleItem>(
+      //                     initialValue: selectedMenu,
+      //                     // Callback that sets the selected popup menu item.
+      //                     onSelected: (SampleItem item) {
+      //                       setState(() {
+      //                         selectedMenu = item;
+      //                       });
+      //                     },
+      //                     itemBuilder: (BuildContext context) => <PopupMenuEntry<SampleItem>>[
+      //                       const PopupMenuItem<SampleItem>(
+      //                         value: SampleItem.itemOne,
+      //                         child: Text(
+      //                           "delete",
+      //                           style: TextStyle(
+      //                             fontWeight: FontWeight.w700,
+      //                             fontSize: 18,
+      //                             color: Colors.black,
+      //                           ),
+      //                         ),
+      //                       ),
+      //                       const PopupMenuItem<SampleItem>(
+      //                         value: SampleItem.itemTwo,
+      //                         child: Text(
+      //                           "Edite",
+      //                           style: TextStyle(
+      //                             fontWeight: FontWeight.w700,
+      //                             fontSize: 18,
+      //                             color: Colors.black,
+      //                           ),
+      //                         ),
+      //                       ),
+      //                     ],
+      //                   ),
+      //                 ),
+      //               ],
+      //             ),
+      //           ),
+      //           const SizedBox(height: 400),
+      //           Row(
+      //             mainAxisAlignment: MainAxisAlignment.center,
+      //             children: <Widget>[
+      //               const SizedBox(width: 280),
+      //               FloatingActionButton(
+      //                 onPressed: () {
+      //                   Navigator.push(
+      //                     context,
+      //                     MaterialPageRoute(
+      //                       builder: (context) => const TodoScreen(),
+      //                     ),
+      //                   );
+      //                 },
+      //                 child: const Icon(Icons.add),
+      //               ),
+      //             ],
+      //           ),
+      //         ],
+      //       ),
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
